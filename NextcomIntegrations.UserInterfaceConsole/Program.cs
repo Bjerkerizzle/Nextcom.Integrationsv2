356ebf2f18219a33f8499f1930c17826e7da3eb8
Bjerkerizzle <55440561+Bjerkerizzle@users.noreply.github.com>
2020-06-29 09:20:11 +0200

    <3

---
+++ NextcomIntegrations.UserInterfaceConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NextcomIntegrations.Application;
+using NextcomIntegrations.Core.ApplicationServices;
+using NextcomIntegrations.Infrastructure.DataAccess;
+using NextcomIntegrations.Infrastructure.Nextcom;
+using NextcomIntegrations.Infrastructure.Tripletex;
+
+namespace NextcomIntegrations.UserInterfaceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App.Run();
+        }
+    }
+}
